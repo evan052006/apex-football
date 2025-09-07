fa42fa3ecd7307652d9d8777bd88fa84a397ce7c
Evan <88143635+evan052006@users.noreply.github.com>
2025-09-07 14:51:05 +0700

    Modified settings.py to allow localhost to test project on local computer

--- apex_football/settings.py
+++ apex_football/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-=15w0b8mr11(8zkfx3m&1(a_6uzz51^_o0n857)v1oh*=$zj$u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
