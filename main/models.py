9a023fffd5549f0216774663a200207072bdbe8b
Evan <88143635+evan052006@users.noreply.github.com>
2025-09-10 10:16:39 +0700

    Created Product Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    CATEGORIES = [
+        ('FW', 'Footwear'),
+        ('Sh', 'Shirts'),
+        ('misc', 'Misc'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    description = models.TextField(max_length=1000)
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.URLField(max_length=20, choices=CATEGORIES, default='FW')
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
