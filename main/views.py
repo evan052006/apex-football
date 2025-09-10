8101f82c6ff42e96974601e927f9c088ae314f41
Evan <88143635+evan052006@users.noreply.github.com>
2025-09-10 09:39:25 +0700

    Routed and displayed views, tested with name, class and app_name

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def show_index(request):
+    return render(
+        request,
+        "index.html",
+        {
+            "nama": "Christopher Evan Tanuwidjaja",
+            "kelas": "A",
+            "app_name": "main"
+        }
+    )
