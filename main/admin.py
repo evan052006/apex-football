4abb0b749b0b92c6d6dd19a7067d7d75fd8ded6d
Evan <88143635+evan052006@users.noreply.github.com>
2025-09-15 12:30:42 +0700

    Created xml and json views and added to urls

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main.models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
