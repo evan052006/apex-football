6da0201d1ea05d969a2a75eb4866af3c561fc667
Evan <88143635+evan052006@users.noreply.github.com>
2025-09-15 12:30:42 +0700

    Created xml and json views and added to urls

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from main.views import show_index
+from main.views import show_index, show_products_by_xml, show_products_by_json, show_product_xml_by_id, show_product_json_by_id
 
 
 urlpatterns = [
     path('', show_index, name='index'),
+    path('xml', show_products_by_xml, name='show_products_by_xml'),
+    path('json', show_products_by_json, name='show_products_by_json'),
+    path('xml/<str:id>/', show_product_xml_by_id, name='show_product_xml_by_id'),
+    path('json/<str:id>/', show_product_json_by_id, name='show_product_json_by_id')
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
+
+
+def show_products_by_xml(request):
+    product_list = Product.objects.all()
+    xml_data = serializers.serialize("xml", product_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+
+def show_products_by_json(request):
+    product_list = Product.objects.all()
+    json_data = serializers.serialize("json", product_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+
+def show_product_xml_by_id(request, id):
+    try:
+        product_item = Product.objects.get(id=id)
+        xml_data = serializers.serialize("xml", [product_item])
+        return HttpResponse(xml_data, content_type="application/xml")
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
+
+
+def show_product_json_by_id(request, id):
+    try:
+        product_item = Product.objects.get(id=id)
+        json_data = serializers.serialize("json", [product_item])
+        return HttpResponse(json_data, content_type="application/json")
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
 
 
 def show_index(request):
