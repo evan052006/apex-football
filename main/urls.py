6deb2a23dda105eff60416518fda49621a4d9de6
Evan <88143635+evan052006@users.noreply.github.com>
2025-09-15 13:01:44 +0700

    Added new button to a form to add new products

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from main.views import show_index, show_products_by_xml, show_products_by_json, show_product_xml_by_id, show_product_json_by_id
+from main.views import show_index, show_products_by_xml, show_products_by_json, show_product_xml_by_id, show_product_json_by_id, create_product
 
+app_name = 'main'
 
 urlpatterns = [
-    path('', show_index, name='index'),
+    path('', show_index, name='show_index'),
+    path('create_product', create_product, name='create_product'),
     path('xml', show_products_by_xml, name='show_products_by_xml'),
     path('json', show_products_by_json, name='show_products_by_json'),
     path('xml/<str:id>/', show_product_xml_by_id, name='show_product_xml_by_id'),
