277f96d22f6ecd620b293b05e686971ffd6f8f34
Evan <88143635+evan052006@users.noreply.github.com>
2025-09-16 14:48:58 +0700

    Finished Tugas 3

--- apex_football/settings.py
+++ apex_football/settings.py
@@ -34,6 +34,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "christopher-evan41-apexfootball.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://christopher-evan41-apexfootball.pbp.cs.ui.ac.id"
+]
+
 
 # Application definition
 
